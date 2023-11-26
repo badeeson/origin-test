c4423e199bd62b744c25dea871b6281f45d8275d
Badeeson Yongyuthmeechai <badeeson@Badeesons-MacBook-Air.local>
2023-11-27 02:34:02 +0700

    Fix error

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,11 @@
 import { Grid, IconButton } from '@mui/material';
 import Link from 'next/link';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import Image from 'next/image';
 
 const videos = [
-  { id: '01', title: 'Video 1', thumbnail: 'thumbnail01.png' },
-  { id: '02', title: 'Video 2', thumbnail: 'thumbnail02.png' },
+  { id: '01', title: 'Video 1', thumbnail: '/thumbnail01.png' },
+  { id: '02', title: 'Video 2', thumbnail: '/thumbnail02.png' },
 ];
 
 const HomePage = () => {
@@ -14,8 +15,8 @@ const HomePage = () => {
       <h1>Welcome to My Video</h1>
       <Grid container spacing={8}>
         {videos.map((video) => (
-          <Grid item xs={12} sm={12} md={12} lg={6} xl={6} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <Link key={video.id} href={`/video/${video.id}`}>
+          <Grid key={video.id} item xs={12} sm={12} md={12} lg={6} xl={6} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Link href={`/video/${video.id}`}>
               <div style={{ position: 'relative', width: 640, height: 360 }}>
                 <div style={{ position: 'absolute', top: '5%', left: '5%' }}>
                   <h2>{video.title}</h2>
@@ -25,7 +26,7 @@ const HomePage = () => {
                     <PlayArrowIcon sx={{ height: 128, width: 128 }} />
                   </IconButton>
                 </div>
-                <img src={video.thumbnail} width="100%" height="405" alt={video.title} style={{ opacity: 0.5 }} />
+                <Image src={video.thumbnail} width="640" height="360" alt={video.title} style={{ opacity: 0.5 }} />
               </div>
             </Link>
           </Grid>
