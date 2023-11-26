42d5d6a9f61e0ef6c04ae74e3cf570c65bd3fe00
Badeeson Yongyuthmeechai <badeeson@Badeesons-MacBook-Air.local>
2023-11-27 02:29:58 +0700

    Reponsive column

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 // pages/index.js
-import { IconButton } from '@mui/material';
+import { Grid, IconButton } from '@mui/material';
 import Link from 'next/link';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
@@ -12,23 +12,25 @@ const HomePage = () => {
   return (
     <div>
       <h1>Welcome to My Video</h1>
-      <div>
+      <Grid container spacing={8}>
         {videos.map((video) => (
-          <Link key={video.id} href={`/video/${video.id}`}>
-            <div style={{ position: 'relative', width: 1280 }}>
-              <div style={{ position: 'absolute', top: '5%', left: '5%' }}>
-                <h2>{video.title}</h2>
+          <Grid item xs={12} sm={12} md={12} lg={6} xl={6} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Link key={video.id} href={`/video/${video.id}`}>
+              <div style={{ position: 'relative', width: 640, height: 360 }}>
+                <div style={{ position: 'absolute', top: '5%', left: '5%' }}>
+                  <h2>{video.title}</h2>
+                </div>
+                <div style={{ position: 'absolute', top: 'calc(50% - 120px)', left: 'calc(50% - 120px)', zIndex: 1 }}>
+                  <IconButton aria-label="play/pause">
+                    <PlayArrowIcon sx={{ height: 128, width: 128 }} />
+                  </IconButton>
+                </div>
+                <img src={video.thumbnail} width="100%" height="405" alt={video.title} style={{ opacity: 0.5 }} />
               </div>
-              <div style={{ position: 'absolute', top: 'calc(50% - 120px)', left: 'calc(50% - 120px)', zIndex: 1 }}>
-                <IconButton aria-label="play/pause">
-                  <PlayArrowIcon sx={{ height: 128, width: 128 }} />
-                </IconButton>
-              </div>
-              <img src={video.thumbnail} width="1280" alt={video.title} style={{ opacity: 0.5 }} />
-            </div>
-          </Link>
+            </Link>
+          </Grid>
         ))}
-      </div>
+      </Grid>
     </div>
   );
 };
